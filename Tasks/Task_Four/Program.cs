1938e84683995bd36ef5eb63132340caa29ac719
Alex_Green <deadpoolweid@yandex.ru>
2015-10-10 18:44:36 +0300

    Task 4 - Done.
    Определить­, превосходи­т ли первый элемент массива из десяти чисел
    среднее значение элементов этого массива.

---
+++ Tasks/Task_Four/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_Four
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Случайно сгенерированный массив
+            int[] array = new int[10];
+            Random random = new Random((int)DateTime.Now.Ticks);
+            for (int i = 0; i < array.Count(); i++)
+            {
+                array[i] = random.Next(0, 50);
+            }
+
+            foreach (var VARIABLE in array)
+            {
+                Console.Write(VARIABLE + "\t");
+            }
+
+            float averageValue = 0;
+
+            foreach (var VARIABLE in array)
+            {
+                averageValue += VARIABLE;
+            }
+            averageValue/=array.Count();
+
+            if (array[0] > averageValue)
+            {
+                Console.WriteLine("Первый элемент массива превосходит среднее значение всех элементов.");
+            }
+            else
+            {
+                Console.WriteLine("Первый элемент массива не превосходит среднее значение всех элементов.");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
