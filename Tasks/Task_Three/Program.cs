78823972e1cbbfa9b9896c0c484ed3d787c4f33c
Alex_Green <deadpoolweid@yandex.ru>
2015-10-10 18:38:16 +0300

    Task 3 - Done.
    В массиве из 10 чисел найти сумму элементов больших, чем   второй
    элемент этого массива.

---
+++ Tasks/Task_Three/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_Three
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Случайно сгенерированный массив
+            int[] array = new int[10];
+            Random random = new Random((int)DateTime.Now.Ticks);
+            for (int i = 0; i < array.Count(); i++)
+            {
+                array[i] = random.Next(0, 50);
+            }
+
+            foreach (var VARIABLE in array)
+            {
+                Console.Write(VARIABLE + "\t");
+            }
+
+            int s = 0;
+
+            foreach (var VARIABLE in array)
+            {
+                if (VARIABLE > array[1])
+                {
+                    s += VARIABLE;
+                }
+            }
+
+            Console.WriteLine("Сумма элементов больших, чем второй элемент массива: " + s);
+
+            Console.ReadKey();
+        }
+    }
+}
