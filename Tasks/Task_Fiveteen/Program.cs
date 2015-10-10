39cc3dc43f9c75b88101d87096e8d66970772d4e
Alex_Green <deadpoolweid@yandex.ru>
2015-10-10 19:48:06 +0300

    Task 15 - Done.
    Найти сумму элементов массива из 10 чисел, меньших, чем 5-й элемент
    этого массива.

---
+++ Tasks/Task_Fiveteen/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_Fiveteen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Случайно сгенерированный массив
+            int[] array = new int[10];
+            Random random = new Random((int)DateTime.Now.Ticks);
+            for (int i = 0; i < array.Count(); i++)
+            {
+                array[i] = random.Next(0, 50);
+            }
+
+            foreach (var VARIABLE in array)
+            {
+                Console.Write(VARIABLE + "\t");
+            }
+
+            int s = 0;
+
+            foreach (var e in array)
+            {
+                if (e < array[4])
+                {
+                    s += e;
+                }
+            }
+
+            Console.WriteLine("Сумма элементов, меньших пятого элемента: " + s);
+
+            Console.ReadKey();
+        }
+    }
+}
