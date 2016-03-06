525449cdc699cc3c62453c744d38b82b7c429a51
Alex_Green <deadpoolweid@yandex.ru>
2016-03-06 19:46:59 +0300

    v1.0 pre alpha
    - Добавлен лаунчер для запуска двух программ

---
+++ Tasks/Launcher/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using RE = RegularExpressions;
+
+namespace Launcher
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            RE.Form1 form1 = new RE.Form1();
+            this.Visible = false;
+            form1.ShowDialog();
+            Visible = true;
+        }
+    }
+}
